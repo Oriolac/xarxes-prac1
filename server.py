cf69441214c4cc993bba8dad8e2cf83d4953f2d2
Oriol <oriolalascercos@gmail.com>
2019-03-08 20:12:23 +0100

    Server implementat files

--- server.py
+++ server.py
@@ -4,18 +4,23 @@ Servidor
 """
 
 import sys
+import time
+import tokenize
 
 
 def print_if_debug(debug, cadena):
     """print_if_debug"""
-    
     if debug:
-        print cadena
+        print time.strftime("%H:%M:%S DEBUG => " + cadena)
+
+def print_if_error(cadena):
+    """ print_if_debug """
+    print time.strftime("%H:%M:%S ERROR => " + cadena)
 
 def lectura_parametres():
     """lectura_parametres"""
 
-    arxius = {"servidor": "server.cfg", "equips": "equips.dat"}
+    nom_arxius = {'servidor': 'server.cfg', 'equips': 'equips.dat'}
     debug = False
     trobat_conf_server = False
     trobat_conf_equips = False
@@ -23,20 +28,40 @@ def lectura_parametres():
     for arg in sys.argv:
         if arg == "-d":
             debug = True
-            print_if_debug(debug, "Hola")
+            print_if_debug(debug, "Mode DEBUG ON.")
         elif arg == "-c":
             trobat_conf_server = True
         elif trobat_conf_server:
-            arxius["servidor"] = arg
+            nom_arxius["servidor"] = arg
+            print_if_debug(debug, "Arxiu de dades de software modificat: " + arg)
             trobat_conf_server = False
         elif arg == "-u":
             trobat_conf_equips = True
         elif trobat_conf_equips:
-            arxius["equips"] = arg
+            nom_arxius["equips"] = arg
+            print_if_debug(debug, "Arxiu d'equips autoritzats modificat: " + arg)
             trobat_conf_equips = False
     if trobat_conf_equips or trobat_conf_server:
-        print "La hemo jodio paco"
-    return (debug, arxius)
+        print "No s'han pogut obrir els arxius modificats"
+        sys.exit()
+    return (debug, obrir_arxius(nom_arxius))
 
-if __name__ == "__main__":
+def obrir_arxius(nom_arxius):
+    """fdsa """
+    try:
+        f_server = open(nom_arxius['servidor'], 'r')
+        f_equip = open(nom_arxius['equips'], 'r')
+    except EnvironmentError:
+        print_if_error("No s'han pogut obrir els arxius.")
+        sys.exit()
+    return {'servidor': f_server, 'equips': f_equip}
+
+def agafar_dades_servidor(fitxer):
+    """ agafar_dades_servidor """
+    lines = fitxer.readlines()
+    for line in lines:
+        tokenize.tokenize(line)
+        
+if __name__ == '__main__':
     DEBUG, ARXIUS = lectura_parametres()
+    agafar_dades_servidor(ARXIUS['servidor'])
